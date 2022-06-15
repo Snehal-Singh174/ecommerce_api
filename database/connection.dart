deb9c41fb8c367d15fefcfa2ee8f159e8d080e4b
Snehal Singh <53808476+Snehal-Singh174@users.noreply.github.com>
2022-06-16 00:26:44 +0530

    changes in connection

--- database/connection.dart
+++ database/connection.dart
@@ -5,12 +5,12 @@ import 'package:postgres/postgres.dart';
 class DB {
   DB() {
     final envVars = Platform.environment;
-    final host = envVars['127.0.0.1'] ?? 'localhost';
+    final host = envVars['DB_HOST'] ?? 'localhost';
     final dbPort = envVars['5432'];
     final port = dbPort == null ? 5432 : int.parse(dbPort);
-    final database = envVars['DB_DATABASE'] ?? 'products';
-    final username = envVars['DB_USERNAME'] ?? 'postgres';
-    final password = envVars['DB_PASSWORD'] ?? 'snehal123';
+    final database = envVars['DB_DATABASE'] ?? 'database';
+    final username = envVars['DB_USERNAME'] ?? 'username';
+    final password = envVars['DB_PASSWORD'] ?? 'password';
     connection = PostgreSQLConnection(
       host,
       port,
